a2fe7acd25cfc740a253fea1fb1bc4512e726525
Apisit <fu.apisit@gmail.com>
2025-04-09 14:46:48 +0700

    feat add API

--- server/app.mjs
+++ server/app.mjs
@@ -1,17 +1,16 @@
 import express from "express";
-import connectionPool  from "./utils/db.mjs";
 
 const app = express();
-app.use(express.json());
+const port = 4001;
 
-const port = 4000;
+app.use(express.json());
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
 
-app.post("/assignment", async (req, res) => {
-  
+app.post("/assignments", async (req, res) => {
+
   const newAssignment = {
     ...req.body,
     created_at: new Date(),
@@ -36,15 +35,98 @@ app.post("/assignment", async (req, res) => {
       ]
     );
   } catch {
-    return res
-      .status(500)
-      .json({
-        message:
-          "Server could not create assignment because database connection",
-      });
+    return res.status(500).json({
+      message: "Server could not create assignment because database connection",
+    });
   }
 
   return res.status(201).json({ message: "Created assignment sucessfully" });
+});
+
+app.get("/assignments", async (req, res) => {
+
+  let results;
+
+  try {
+    results = await connectionPool.query(`select * from assignments`);
+  } catch {
+    return res.status(500).json({
+      message: "Server could not read assignment because database issue",
+    });
+  }
+
+  return res.status(200).json({
+    data: results.rows,
+  });
+});
+
+app.get("/assignments/:assignmentId", async (req, res) => {
+
+  const assignmentIdFromClient = req.params.assignmentId;
+
+  const results = await connectionPool.query(
+    `
+		select * from assignments where assignment_id=$1
+	`,
+    [assignmentIdFromClient]
+  );
+
+  if (!results.rows[0]) {
+    return res.status(404).json({
+      message: `Server could not find a requested assignment (assignment id: ${assignmentIdFromClient})`,
+    });
+  }
+
+  return res.status(200).json({
+    data: results.rows[0],
+  });
+});
+
+app.put("/assignments/:assignmentId", async (req, res) => {
+
+  const assignmentIdFromClient = req.params.assignmentId;
+  const updatedAssignment = { ...req.body, updated_at: new Date() };
+
+  await connectionPool.query(
+    `
+      update assignments
+      set title = $2,
+          content = $3,
+          category = $4,
+          length = $5,
+          status = $6,
+          updated_at = $7
+      where assignment_id = $1
+    `,
+    [
+      assignmentIdFromClient,
+      updatedAssignment.title,
+      updatedAssignment.content,
+      updatedAssignment.category,
+      updatedAssignment.length,
+      updatedAssignment.status,
+      updatedAssignment.updated_at,
+    ]
+  );
+
+  return res.status(200).json({
+    message: "Updated assignment sucessfully",
+  });
+});
+
+app.delete("/assignments/:assignmentId", async (req, res) => {
+
+  const assignmentIdFromClient = req.params.assignmentId;
+
+  await connectionPool.query(
+    `delete from assignments
+	   where assignmentId = $1`,
+    [assignmentIdFromClient]
+  );
+
+  return res.status(200).json({
+    message: "Delete assignment sucessfully",
+  });
 });
 
 app.listen(port, () => {
